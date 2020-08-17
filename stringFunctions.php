17f7dcc74fe5140535f3d284e00f3834f9906a0f
Edgar <62921798+essolares@users.noreply.github.com>
2020-08-16 18:51:21 -0700

    Create stringFunctions.php

---
+++ stringFunctions.php
@@ -0,0 +1,43 @@
+<?php
+
+$str1 = "text <p>numbrer</p> 1";
+$arr= [12,4,5,6,8,3,1];
+
+//crypt - hash streing
+//echo crypt($str1);
+
+//explode - string to array
+//print_r(explode(" ",$str1));
+
+//impolode - array to string
+//print_r(implode($arr,"-"));
+
+//nl2br - insert <br /> over \n
+//echo nl2br($str1);
+
+//sprintf - create format over a formated text
+/*$formato = 'Hay monos en el %s';
+echo sprintf($formato,$str1);
+echo sprintf("%'.9d\n", 123);*/
+
+//strip_tags - remove html tags
+//echo strip_tags($str1);
+
+//str_replace (search, replace, str) -- locate and replace str
+//echo str_replace("1","uno",$str1);
+
+//strpos (str, needle) -- find pos of str
+//echo strpos($str1,"1");
+
+//strrev (str) -- reverse string
+//echo strrev($str1);
+
+//strstr (str, needle) -- return str after find element
+//echo strstr($str1,"numbre");
+
+//strtolower (str) strtoupper - lower and upper
+/*echo strtolower($str1);
+echo strtoupper($str1);*/
+
+//substr (string, start, len) --
+//echo substr($str1,0,5);
